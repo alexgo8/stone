93cae7d5d3772f189343285eeb37dbac8b2c9e3e
Alex Goncharov <461811211@mail.ru>
2024-01-23 16:48:48 +0300

    alex23.01.2024.16:48

--- database/migrations/2024_01_23_113720_create_stone_tag_table.php
+++ database/migrations/2024_01_23_113720_create_stone_tag_table.php
@@ -6,22 +6,32 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('stone_tag', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('stone_tag', function (Blueprint $table) {
+      $table->id();
+      $table->unsignedBigInteger('stone_id');
+      $table->unsignedBigInteger('tag_id');
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('stone_tag');
-    }
+      $table->index('stone_id', 'stone_tag_stone_idx');
+      $table->index('tag_id', 'stone_tag_tag_idx');
+
+      $table->foreign('stone_id', 'stone_tag_note_fk')->on('stones')->references('id');
+      $table->foreign('tag_id', 'stone_tag_tag_fk')->on('tags')->references('id');
+
+
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('stone_tag');
+  }
 };
